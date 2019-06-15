d922c0ea4e5797ed369f260c2a36cd4f046f66f8
JustTams <sarg.tier1@gmail.com>
2019-06-15 03:28:32 -0500

    fixed imports

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Header from "./header"
+import Header from "./header/header"
 import Footer from "./footer/footer"
 import layoutStyles from "./layout.module.scss"
 
