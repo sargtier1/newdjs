1729d1f2443d4089cd0c68fb82c06ce741106972
JustTams <sarg.tier1@gmail.com>
2019-06-15 02:33:08 -0500

    refactored footer with styled components

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,62 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+import styled from "styled-components"
+
+import "./footer.css"
+
+const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+          social {
+            name
+            link
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <StyledFooter>
+      <p>Created by {data.site.siteMetadata.author} | © 2019</p>
+      <StyledList>
+        {data.site.siteMetadata.social &&
+          data.site.siteMetadata.social.map(items => {
+            return (
+              <StyledListItem key={items.name}>
+                <a href={items.link} target="_blank" rel="noopener noreferrer">
+                  {" "}
+                  {items.name}{" "}
+                </a>
+              </StyledListItem>
+            )
+          })}
+      </StyledList>
+    </StyledFooter>
+  )
+}
+
+const StyledFooter = styled.footer`
+  margin-top: 3rem;
+  text-align: center;
+`
+
+const StyledList = styled.ul`
+  margin: 1rem 0;
+  padding: 0;
+  text-align: center;
+`
+
+const StyledListItem = styled.li`
+  display: inline-block;
+  margin: 1rem 2rem;
+  padding: 0;
+  vertical-align: middle;
+
+`
+
+export default Footer
