210b2263b54820e54a95313a988b963106aa6bbb
JustTams <sarg.tier1@gmail.com>
2019-06-15 03:24:32 -0500

    removed an import not being used

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
 import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
