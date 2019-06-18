b61c49f37f10af26779f341d66dba826f2e53405
JustTams <sarg.tier1@gmail.com>
2019-06-18 10:10:56 -0500

    added styled divs for the page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,37 +1,45 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 
-import Img from 'gatsby-image'
+import Img from "gatsby-image"
 
 import Layout from "../components/layout/layout"
 import Head from "../components/head"
 
+import styled from "styled-components"
+
 const AboutPage = () => {
   const data = useStaticQuery(graphql`
-  query {
-    contentfulAboutPage {
-      title
-      image {
+    query {
+      contentfulAboutPage {
         title
-        fluid {
-          ...GatsbyContentfulFluid
+        image {
+          title
+          fluid {
+            ...GatsbyContentfulFluid
+          }
         }
       }
     }
-  }
   `)
 
   return (
     <Layout>
       <Head title="About" />
       <h1>{data.contentfulAboutPage.title}</h1>
-      <Img  fluid={data.contentfulAboutPage.image.fluid}/>
-      <br/>
+      <ImageContainer>
+        <Img fluid={data.contentfulAboutPage.image.fluid} />
+      </ImageContainer>
       <p>
         Want to work with me? <Link to="/contact">Reach out.</Link>
       </p>
     </Layout>
   )
 }
+
+const ImageContainer = styled.div`
+  position: fixed;
+  width: 750px
+`
 
 export default AboutPage
